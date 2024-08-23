04ccb4fe5a6264d2b21cff443bf748b76a738f16
Naser Jawas <naser.jawas@gmail.com>
2024-08-23 14:45:23 +0700

    alexnet from pytorch web

--- simple/model/alexnet.py
+++ simple/model/alexnet.py
@@ -3,39 +3,46 @@ from torch.nn import Sequential
 from torch.nn import Conv2d
 from torch.nn import ReLU
 from torch.nn import MaxPool2d
+from torch.nn import AdaptiveAvgPool2d
 from torch.nn import Dropout
 from torch.nn import Linear
+from torch import flatten
+
 
 class AlexNet(Module):
     def __init__(self, classes):
         super(AlexNet, self).__init__()
-        self.feature = Sequential(
-            Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=1),
+
+        self.features = Sequential(
+            Conv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2),
             ReLU(inplace=True),
-            Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            MaxPool2d(kernel_size=3, stride=2),
+            Conv2d(in_channels=64, out_channels=192, kernel_size=5, padding=2),
             ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=2),
-            Conv2d(in_channels=64, out_channels=96, kernel_size=3, padding=1),
+            MaxPool2d(kernel_size=3, stride=2),
+            Conv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1),
             ReLU(inplace=True),
-            Conv2d(in_channels=96, out_channels=64, kernel_size=3, padding=1),
+            Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
             ReLU(inplace=True),
-            Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1),
+            Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
             ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=1),
+            MaxPool2d(kernel_size=3, stride=2),
         )
+        self.avgpool = AdaptiveAvgPool2d((6, 6))
         self.classifier = Sequential(
-            Dropout(),
-            Linear(in_features=32*12*12, out_features=2048),
+            Dropout(p=0.5),
+            Linear(in_features=256 * 6 * 6, out_features=4096),
             ReLU(inplace=True),
-            Dropout(),
-            Linear(in_features=2048, out_features=1024),
+            Dropout(p=0.5),
+            Linear(in_features=4096, out_features=4096),
             ReLU(inplace=True),
-            Linear(in_features=1024, out_features=classes),
+            Linear(in_features=4096, out_features=classes),
         )
 
     def forward(self, x):
-        x = self.feature(x)
-        x = x.view(-1, 32*12*12)
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = flatten(x, 1)
         output = self.classifier(x)
 
         return output
