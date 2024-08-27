93b3cf703a3f738103171aa1842a2e5c363c3cf3
Naser Jawas <naser.jawas@gmail.com>
2024-08-27 20:22:46 +0700

    data class

--- simple/data/data.py
+++ simple/data/data.py
@@ -1,11 +1,13 @@
+import os
 import torch
 
 from torch.utils.data import Dataset
 
 class Data(Dataset):
-    def __init__(self, list_ids, labels):
-        self.labels = labels
+    def __init__(self, list_ids, img_dir, labels):
         self.list_ids = list_ids
+        self.img_dir = img_dir
+        self.labels = labels
 
     def __len__(self):
         return len(self.list_ids)
