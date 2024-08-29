44fdce4b13fcb061ebdfc4a4d3190490e0291ef5
Naser Jawas <naser.jawas@gmail.com>
2024-08-29 19:44:13 +0700

    data

--- simple/data/data.py
+++ simple/data/data.py
@@ -6,8 +6,30 @@ class ImageDataset(Dataset):
         self.image_folder = image_folder
         self.images = os.listdir(image_folder)
 
+
     def __getitem__(self, idx):
         image_file = self.images[idx]
 
     def __len__(self):
         return len(self.images)
+
+"""
+Notes dataset
+
+the dataset contains:
+    - train:
+    - dev:
+    - test:
+
+    each data folder has:
+    - label
+    - folder of sample.
+
+
+    train/word1/sample1/
+    train/word1/sample2/
+    train/word1/sample3/
+    train/word2/sample1/
+    train/word2/sample2/
+    train/word2/sample3/
+"""
